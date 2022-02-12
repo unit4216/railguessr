a4cb5b5876a602e230f32698ad7c00c57d2156b3
unit4216 <pf.paliza@gmail.com>
2022-02-12 09:36:05 -0800

    cleaned code, first pass

--- script.js
+++ script.js
@@ -27,53 +27,50 @@ var countryList = {
     'lt':'Lithuania',
 }
 
+//init Animate On Scroll
 AOS.init();
 
+//set position for Alertify
 alertify.set('notifier','position', 'top-center');
 
+//get initial photo
 getPhoto();
 
-//JQuery which detects if Enter is pushed (preventDefault keeps page from refreshing before executing checkAnswer)
+//submit guess on enter
 $('#countryInput').keypress(function(e) {
     if(e.which == 13) {
-        e.preventDefault(); //critical to function properly!
+        e.preventDefault(); 
         checkAnswer();
     }
 });
-
-
 
 async function getPhoto() {
 
     //get random country
     const countriesAbbrev = Object.keys(countryList)
     const randomCountry = countriesAbbrev[Math.floor(Math.random() * countriesAbbrev.length)]
-    nameToCheck = countryList[randomCountry] //not using var type makes the var GLOBAL
-    listToCheck = nameToCheck.split(",")
+    nameToCheck = countryList[randomCountry] //global variable assignment
+    listToCheck = nameToCheck.split(",")    //used for when there are multiple correct answers i.e. US, USA, The United States
 
-    //pull all JSON data from country
+    //pull all train station photos for the random country
     const res = await fetch(`https://api.railway-stations.org/${randomCountry}/stations?hasPhoto=True`)
-    const data = await res.json();
-    const text = JSON.stringify(data);
-    const obj = JSON.parse(text);
+    const obj = await res.json();
 
     //pick random object (train station) from JSON array
-    const station = obj[Math.floor(Math.random() * obj.length)];
-    const station_strings = JSON.stringify(station);
-    const stationObj = JSON.parse(station_strings);
+    const stationObj = obj[Math.floor(Math.random() * obj.length)];
 
     //extract photo's URL
     const photoURL = stationObj.photoUrl;
 
-    //Jquery Zoom
+    //init Jquery.zoom
     $(document).ready(function(){
         $('#railPhoto').zoom({magnify: 1});
     });
 
     //send image to div, displaying it on page
-    document.getElementById('railPhoto').innerHTML = `<img src=${photoURL} class='shadow rounded' style='display:block;margin-left:auto;margin-right:auto;height:70%;width:70%'>`;
+    document.getElementById('railPhoto').innerHTML = `<img src=${photoURL} class='shadow rounded' id='image'>`;
 
-    //return country name
+    //print country name in console
     console.log(nameToCheck);
 }
 
@@ -83,18 +80,18 @@ function checkAnswer(){
     var countryInput = document.getElementById('countryInput').value;
 
     if(listToCheck.includes(countryInput) == true){
-        //alert('Correct!');
-        alertify.success('Correct!');
 
+        alertify.success('Correct!');
         $('#countryInput').val(''); //reset input box
         getPhoto(); //get next photo
+
     }
     else if(listToCheck.includes(countryInput) == false){
-        //alert(`False! The correct answer is ${nameToCheck}`);
-        alertify.error(`False! The correct answer is ${listToCheck[0]}`);
 
+        alertify.error(`False! The correct answer is ${listToCheck[0]}`);
         $('#countryInput').val(''); //reset input box
         getPhoto(); //get next photo
+
     }
 
 }
